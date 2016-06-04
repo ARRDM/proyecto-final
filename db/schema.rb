d5d2193542918566b4c1a89874f80650e9356e19
rickjmzmnz <rickjmzmnz@ciencias.unam.mx>
2016-06-04 09:27:08 -0500

    Agrego mas campos para el registro de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529162925) do
+ActiveRecord::Schema.define(version: 20160604132819) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20160529162925) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "image"
+    t.string   "user_name"
+    t.string   "user_lastname_p"
+    t.string   "user_lastname_m"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
